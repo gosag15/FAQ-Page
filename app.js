2d7c38d4259acd7db44cde3a657cec650e9842a9
gosag15 <gagbonlahor173@gmail.com>
2021-07-04 19:53:31 +0900

    Questions hidden on page load

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const questions = document.querySelectorAll(".accordion");
+const questions = document.querySelectorAll(".accordion");//selects all questions
 function revealAnswer(){
-    const Answer = this.nextElementSibling;
+    const Answer = this.nextElementSibling;//selects the answer for each question
     if(Answer.style.display === "block"){
         Answer.style.display = "none"
     }else{
